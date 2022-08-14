30d93f77400f059873b0fdc9654ee58bc9dc44de
SirG <sirgittarus@gmail.com>
2022-08-14 11:52:26 +0100

    fixing codacy errors

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 const limiter = rateLimit({
-  windowMs: 1000, // 1 second in milliseconds
-  max: 3,
-  message: "Too many requests from this IP, please try again after 3 seconds",
-  standardHeaders: true,
-  legacyHeaders: false,
+    windowMs: 1000, // 1 second in milliseconds
+    max: 3,
+    message: "Too many requests from this IP, please try again after 3 seconds",
+    standardHeaders: true,
+    legacyHeaders: false,
 });
 // apply to all requests
 app.use(limiter);
@@ -26,27 +26,26 @@ app.get("/", (req, res) => {
        .send("Welcome to TalentQL Backend Assessment API")
 })
 app.get("/howold", check("dob").notEmpty().isDate(), async(req, res) => {
-    
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-          return res.status(400)
-                    .json({ error: `${errors.array()[0]["msg"]}, please enter a valid date of birth in the format YYYY-MM-DD.`});
-        }
-        let today = new Date();
-        let dob = new Date(req.query.dob);// Get date of birth and extract month and year
-        let yearOfBirthInMilliseconds = dob.getTime();
-        let currentYearInMilliseconds = today.getTime();
-        let oneYearInMilliseconds = 1000 * 60 * 60 * 24 * 365;
-        if(yearOfBirthInMilliseconds > currentYearInMilliseconds){
-            return res.status(400)
-                      .json({ error: "Date of birth can't be more than the current year", });
-        }
-        let age = Math.round((currentYearInMilliseconds - yearOfBirthInMilliseconds)/oneYearInMilliseconds);
-        return res.status(200)
-                  .send({"message": "age calculated successfully","age": age});
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400)
+              .json({ error: `${errors.array()[0]["msg"]}, please enter a valid date of birth in the format YYYY-MM-DD.`});
+    }
+    let today = new Date();
+    let dob = new Date(req.query.dob);// Get date of birth and extract month and year
+    let yearOfBirthInMilliseconds = dob.getTime();
+    let currentYearInMilliseconds = today.getTime();
+    let oneYearInMilliseconds = 1000 * 60 * 60 * 24 * 365;
+    if(yearOfBirthInMilliseconds > currentYearInMilliseconds){
+        return res.status(400)
+                    .json({ error: "Date of birth can't be more than the current year", });
+    }
+    let age = Math.round((currentYearInMilliseconds - yearOfBirthInMilliseconds)/oneYearInMilliseconds);
+    return res.status(200)
+                .send({"message": "age calculated successfully","age": age});
   
 });
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}`)
+  console.log(`App running on port ${PORT}`)
 });
 
