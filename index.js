fc90deb06443d8c5c699395cdb6d169c7117e9b8
SirG <sirgittarus@gmail.com>
2022-08-14 12:02:03 +0100

    fixing codacy errors

--- index.js
+++ index.js
@@ -27,8 +27,7 @@ app.get("/", (req, res) => {
 app.get("/howold", check("dob").notEmpty().isDate(), async(req, res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()){
-        return res.status(400)
-              .json({ error: `${errors.array()[0]["msg"]}, please enter a valid date of birth in the format YYYY-MM-DD.`});
+        return res.status(400).json({ error: `${errors.array()[0]["msg"]}, please enter a valid date of birth in the format YYYY-MM-DD.`});
     }
     let today = new Date();
     let dob = new Date(req.query.dob);// Get date of birth and extract month and year
@@ -36,15 +35,12 @@ app.get("/howold", check("dob").notEmpty().isDate(), async(req, res) => {
     let currentYearInMilliseconds = today.getTime();
     let oneYearInMilliseconds = 1000 * 60 * 60 * 24 * 365;
     if(yearOfBirthInMilliseconds > currentYearInMilliseconds){
-        return res.status(400)
-                    .json({ error: "Date of birth can't be more than the current year", });
+        return res.status(400).json({ error: "Date of birth can't be more than the current year", });
     }
     let age = Math.round((currentYearInMilliseconds - yearOfBirthInMilliseconds)/oneYearInMilliseconds);
-    return res.status(200)
-                .send({"message": "age calculated successfully","age": age});
+    return res.status(200).send({ message: "age calculated successfully", age: age});
   
 });
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}`)
-});
+  console.log(`App running on port ${PORT}`);});
 
