e3bfe8a4543cd4e363e9c4004f569a87ff31c7c1
SirG <sirgittarus@gmail.com>
2022-08-16 20:53:24 +0100

    added urlencoded

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(cors());
 
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 const PORT = process.env.PORT || 3000;
 const limiter = rateLimit({
