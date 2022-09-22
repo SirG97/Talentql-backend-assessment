ebe93346a545c19a2d998d1d12e1b8bea0e57859
SirG <sirgittarus@gmail.com>
2022-09-22 10:11:55 +0100

    reverting to the old code to fish out error

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import express from 'express';
-// const rateLimit = require("express-rate-limit");
+import rateLimit from 'express-rate-limit';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import requestIP from 'request-ip';
-import nodeCache from 'node-cache';
-import {isIP, isIPv4, isIPv6} from 'is-ip';
+// import requestIP from 'request-ip';
+// import nodeCache from 'node-cache';
+// import {isIP, isIPv4, isIPv6} from 'is-ip';
 import { validationResult, check } from 'express-validator';
 
 //enable access to environment Variables
@@ -19,68 +19,67 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 
-const TIME_FRAME_IN_S = 10;
-const TIME_FRAME_IN_MS = TIME_FRAME_IN_S * 1000;
-const MS_TO_S = 1 / 1000;
-const RPS_LIMIT = 2;
-
+// const TIME_FRAME_IN_S = 10;
+// const TIME_FRAME_IN_MS = TIME_FRAME_IN_S * 1000;
+// const MS_TO_S = 1 / 1000;
+// const RPS_LIMIT = 2;
 
 const PORT = process.env.PORT || 3000;
-// const limiter = rateLimit({
-//   windowMs: 1000, // 1 second
-//   max: 3,
-//   message: "Too many requests from this IP, please try again after 3 seconds",
-//   standardHeaders: true,
-//   legacyHeaders: false,
-//       handler: (req, res, next, options) => {
-//         res.status(options.statusCode).json({
-//             status: 'error',
-//             error: 'Too many requests, please try again later.'
-//         });
-//     }
-// });
-// // apply to all requests
-// app.use(limiter);
-const ipMiddleware = async function (req, res, next) {
-    let clientIP = requestIP.getClientIp(req);
-    if (isIPv6(clientIP)) {
-        clientIP = clientIP.split(':').splice(0, 4).join(':') + '::/64';
-    }
-   
-    updateCache(clientIP);
-    const IPArray = IPCache.get(clientIP);
-    if (IPArray.length > 1) {
-        const rps = IPArray.length / ((IPArray[IPArray.length - 1] - IPArray[0]) * MS_TO_S);
-        if (rps > RPS_LIMIT) {
-            return res.status(429).json({
-                            status: 'error',
-                            error: 'Too many requests, please try again later.'
-                        });
-            console.log('You are hitting limit', clientIP);
-        }
-    }
-    next();
-};
-app.use(ipMiddleware);
-
-const IPCache = new nodeCache({ stdTTL: TIME_FRAME_IN_S, deleteOnExpire: false, checkperiod: TIME_FRAME_IN_S });
-IPCache.on('expired', (key, value) => {
-    if (new Date() - value[value.length - 1] > TIME_FRAME_IN_MS) {
-        IPCache.del(key);
-    }else {
-        const updatedValue = value.filter(function (element) {
-            return new Date() - element < TIME_FRAME_IN_MS;
+const limiter = rateLimit({
+  windowMs: 60000, // 1 second
+  max: 3,
+  message: "Too many requests from this IP, please try again after 3 seconds",
+  standardHeaders: true,
+  legacyHeaders: false,
+      handler: (req, res, next, options) => {
+        res.status(429).json({
+            status: 'error',
+            error: 'Too many requests, please try again later.'
         });
-        IPCache.set(key, updatedValue, TIME_FRAME_IN_S - (new Date() - updatedValue[0]) * MS_TO_S);
     }
-
 });
+// apply to all requests
+app.use(limiter);
+// const ipMiddleware = async function (req, res, next) {
+//     let clientIP = requestIP.getClientIp(req);
+//     if (isIPv6(clientIP)) {
+//         clientIP = clientIP.split(':').splice(0, 4).join(':') + '::/64';
+//     }
+   
+//     updateCache(clientIP);
+//     const IPArray = IPCache.get(clientIP);
+//     if (IPArray.length > 1) {
+//         const rps = IPArray.length / ((IPArray[IPArray.length - 1] - IPArray[0]) * MS_TO_S);
+//         if (rps > RPS_LIMIT) {
+//             return res.status(429).json({
+//                             status: 'error',
+//                             error: 'Too many requests, please try again later.'
+//                         });
+//             console.log('You are hitting limit', clientIP);
+//         }
+//     }
+//     next();
+// };
+// app.use(ipMiddleware);
 
-const updateCache = (ip) => {
-    let IPArray = IPCache.get(ip) || [];
-    IPArray.push(new Date());
-    IPCache.set(ip, IPArray, (IPCache.getTtl(ip) - Date.now()) * MS_TO_S || TIME_FRAME_IN_S);
-};
+// const IPCache = new nodeCache({ stdTTL: TIME_FRAME_IN_S, deleteOnExpire: false, checkperiod: TIME_FRAME_IN_S });
+// IPCache.on('expired', (key, value) => {
+//     if (new Date() - value[value.length - 1] > TIME_FRAME_IN_MS) {
+//         IPCache.del(key);
+//     }else {
+//         const updatedValue = value.filter(function (element) {
+//             return new Date() - element < TIME_FRAME_IN_MS;
+//         });
+//         IPCache.set(key, updatedValue, TIME_FRAME_IN_S - (new Date() - updatedValue[0]) * MS_TO_S);
+//     }
+
+// });
+
+// const updateCache = (ip) => {
+//     let IPArray = IPCache.get(ip) || [];
+//     IPArray.push(new Date());
+//     IPCache.set(ip, IPArray, (IPCache.getTtl(ip) - Date.now()) * MS_TO_S || TIME_FRAME_IN_S);
+// };
 
 app.get("/", (req, res) => {
     res.status(200)
