d1798a72eff7dbfc23a5645cfb7173ebec4c1f2e
SirG <sirgittarus@gmail.com>
2022-09-24 16:28:23 +0100

    clean up the code

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ const express = require('express');
 const rateLimit = require('express-rate-limit');
 const { config } = require('dotenv');
 const cors = require('cors');
-// import requestIP from 'request-ip';
-// import nodeCache from 'node-cache';
-// import {isIP, isIPv4, isIPv6} from 'is-ip';
+
 const { validationResult, check } = require('express-validator');
 
 //enable access to environment Variables
 config();
 
 const app = express();
+
 app.set('trust proxy', 2)
 // adds middleware for cross-origin resource sharing configuration
 app.use(cors());
@@ -18,11 +17,6 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.use(express.json());
 
-
-// const TIME_FRAME_IN_S = 10;
-// const TIME_FRAME_IN_MS = TIME_FRAME_IN_S * 1000;
-// const MS_TO_S = 1 / 1000;
-// const RPS_LIMIT = 2;
 
 const PORT = process.env.PORT || 3000;
 const limiter = rateLimit({
@@ -43,46 +37,6 @@ const limiter = rateLimit({
 // apply to all requests
 
 app.use(limiter);
-// const ipMiddleware = async function (req, res, next) {
-//     let clientIP = requestIP.getClientIp(req);
-//     if (isIPv6(clientIP)) {
-//         clientIP = clientIP.split(':').splice(0, 4).join(':') + '::/64';
-//     }
-   
-//     updateCache(clientIP);
-//     const IPArray = IPCache.get(clientIP);
-//     if (IPArray.length > 1) {
-//         const rps = IPArray.length / ((IPArray[IPArray.length - 1] - IPArray[0]) * MS_TO_S);
-//         if (rps > RPS_LIMIT) {
-//             return res.status(429).json({
-//                             status: 'error',
-//                             error: 'Too many requests, please try again later.'
-//                         });
-//             console.log('You are hitting limit', clientIP);
-//         }
-//     }
-//     next();
-// };
-// app.use(ipMiddleware);
-
-// const IPCache = new nodeCache({ stdTTL: TIME_FRAME_IN_S, deleteOnExpire: false, checkperiod: TIME_FRAME_IN_S });
-// IPCache.on('expired', (key, value) => {
-//     if (new Date() - value[value.length - 1] > TIME_FRAME_IN_MS) {
-//         IPCache.del(key);
-//     }else {
-//         const updatedValue = value.filter(function (element) {
-//             return new Date() - element < TIME_FRAME_IN_MS;
-//         });
-//         IPCache.set(key, updatedValue, TIME_FRAME_IN_S - (new Date() - updatedValue[0]) * MS_TO_S);
-//     }
-
-// });
-
-// const updateCache = (ip) => {
-//     let IPArray = IPCache.get(ip) || [];
-//     IPArray.push(new Date());
-//     IPCache.set(ip, IPArray, (IPCache.getTtl(ip) - Date.now()) * MS_TO_S || TIME_FRAME_IN_S);
-// };
 const validateDob = (req, res, next) => {
     const { query: { dob } } = req; //destructure dob from query params
 
@@ -111,13 +65,9 @@ app.get("/", (req, res) => {
 })
 // check("dob").notEmpty().isDate()
 app.get("/howold", validateDob, async(req, res) => {
-    const errors = validationResult(req);
-    // if(!errors.isEmpty()){
-    //     return res.status(400).json({ error: `${errors.array()[0]["msg"]}, please enter a valid date of birth in the format YYYY-MM-DD.`});
-    // }
+
     let today = new Date();
-    let dob = new Date(req.query.dob);// Get date of birth and extract month and year
-    let yearOfBirthInMilliseconds = dob.getTime();
+   
     let currentYearInMilliseconds = today.getTime();
     let oneYearInMilliseconds = 1000 * 60 * 60 * 24 * 365;
     if(req.validDob > currentYearInMilliseconds){
